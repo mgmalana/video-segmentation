87e10c68132454610a3acc3f02dc6f74ed46126f
mgmalana <mgmalana@gmail.com>
2016-11-11 18:32:18 +0800

    Initialize Javafx project

--- src/VideoSegmentation.java
+++ src/VideoSegmentation.java
@@ -1,5 +1,6 @@
 import com.sun.image.codec.jpeg.ImageFormatException;
 import model.Image;
+import videoSegmentationMethods.HistogramComparison;
 
 import java.io.File;
 import java.io.IOException;
@@ -13,6 +14,7 @@ public class VideoSegmentation {
     public void videoSegment(File selectedDirectory){
         //traverse files of the selectedDirectory. if image. save the image
         ArrayList<Image> images =  new ArrayList<>();
+        HistogramComparison hc = new HistogramComparison();
 
         //adds all the images to the array.
         for (File file : selectedDirectory.listFiles()) {
@@ -23,6 +25,14 @@ public class VideoSegmentation {
             } catch (IOException e) {
                 e.printStackTrace();
             }
+        }
+
+        for(int i = 0; i < images.size() - 1; i++){
+            System.out.println("Distance of " + images.get(i).getFile().getName() +
+                    " to " + images.get(i + 1).getFile().getName() + " is: " +
+                    hc.getDistance(images.get(i), images.get(i + 1)));
+
+
         }
     }
 }
