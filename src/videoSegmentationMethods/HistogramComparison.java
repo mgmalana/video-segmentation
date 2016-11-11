073e15c507054f47fecb5bc9652cfbc8eb0a9897
mgmalana <mgmalana@gmail.com>
2016-11-11 16:12:21 +0800

    HistogramComparison of Median Grid

--- src/videoSegmentationMethods/HistogramComparison.java
+++ src/videoSegmentationMethods/HistogramComparison.java
@@ -2,33 +2,72 @@ package videoSegmentationMethods;
 
 import model.Image;
 
+import java.util.Arrays;
+
 /**
  * Created by mgmalana on 10/11/2016.
  */
 public class HistogramComparison {
     public static final int NUM_COLORS = 64;
+    private static final int NUM_GRID = 4; //4x4
 
     public int getDistance(Image firstImage, Image nextImage){
-        int distance = 0;
-        int[] firstHistogram = getHistogram(firstImage.getRGBMatrix());
-        int[] nextHistogram = getHistogram(nextImage.getRGBMatrix());
+        return getDistance(firstImage.getRGBMatrix(), nextImage.getRGBMatrix());
+    }
 
-        for (int i = 0; i < NUM_COLORS; i++){
-            distance+= Math.abs(firstHistogram[i] - nextHistogram[i]);
+    private int getDistance(int[][] firstImageMatrix, int[][] nextImageMatrix) {
+        int[][] firstImageHistograms = getGridHistograms(firstImageMatrix);
+        int[][] nextImageHistograms = getGridHistograms(nextImageMatrix);
+        int[] distances = new int[firstImageHistograms.length];
+        int distance = 0;
+
+        for(int i = 0; i < firstImageHistograms.length; i++){
+            distances[i] = getDistanceOfHistograms(firstImageHistograms[i], nextImageHistograms[i]);
+        }
+
+        Arrays.sort(distances);
+
+        for(int i = NUM_GRID; i < distances.length - NUM_GRID; i++) { //only gets the median distances
+            distance += distances[i];
         }
 
         return distance;
     }
 
-    private int[] getHistogram(int[][] rgbMatrix) {
-        int[] histogramMap = new int[NUM_COLORS];
+    private int getDistanceOfHistograms(int[] firstImageHistogram, int[] nextImageHistogram) {
+        int distance = 0;
+        for (int i = 0; i < NUM_COLORS; i++){
+            distance+= Math.abs(firstImageHistogram[i] - nextImageHistogram[i]);
+        }
 
-        //traverses the matrix and increments the color counter
-        for (int[] lArray: rgbMatrix) {
-            for (int luv: lArray) {
-                histogramMap[luv]++;
+        return distance;
+    }
+
+    private int[][] getGridHistograms(int[][] matrix) {
+        int[][] imageGrid = new int[NUM_GRID * NUM_GRID][]; //int[x][y] = x is number of grid, y is histogram color
+        int width = matrix.length;
+        int height = matrix[0].length;
+
+        for (int i = 0; i < NUM_GRID; i++){
+            for (int j = 0; j < NUM_GRID; j++){
+                imageGrid[i * NUM_GRID + j] = getHistogram(matrix,
+                        (int)((1.0*i / NUM_GRID) * width), (int)((1.0*j / NUM_GRID) * height),
+                        (int)(((1.0*i + 1)/ NUM_GRID) * width), (int)(((1.0*j + 1)/ NUM_GRID) * height));
             }
         }
+
+        return imageGrid;
+    }
+
+    private int[] getHistogram(int[][] rgbMatrix, int x1, int y1, int x2, int y2) {
+        int[] histogramMap = new int[NUM_COLORS];
+
+        for (int i = x1; i < x2; i++){
+            for (int j = y1; j < y2; j++){
+                histogramMap[rgbMatrix[i][j]]++;
+            }
+        }
+
         return histogramMap;
     }
 }
